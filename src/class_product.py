d69832cb3a2cc476c09c25fb3714fede8d52b084
Anton Mosyagin <mosyagin@yandex.ru>
2026-05-31 09:35:58 +0500

    new file src/class_product.py

---
+++ src/class_product.py
@@ -0,0 +1,13 @@
+import json
+from typing import List
+
+
+class Product:
+    """Класс, представляющий продукт."""
+
+    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
+        self.name = name
+        self.description = description
+        self.price = float(price)
+        self.quantity = quantity
+
