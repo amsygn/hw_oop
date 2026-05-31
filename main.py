6d9c63595d7d7c6898113f3558e93d4c7901f585
Anton Mosyagin <mosyagin@yandex.ru>
2026-05-31 09:40:47 +0500

    create src/from_jason.py

--- main.py
+++ main.py
@@ -34,34 +34,6 @@ product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
 
-def load_categories_from_json(file_path: str) -> List[Category]:
-    """Доп.задание: загружает данные из JSON файла и создает объекты Category и Product."""
-    with open(file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-
-    categories: list[Category] = []
-
-    for category_data in data:
-        products = []
-        for product_data in category_data.get('products', []):
-            product = Product(
-                name=product_data['name'],
-                description=product_data['description'],
-                price=product_data['price'],
-                quantity=product_data['quantity']
-            )
-            products.append(product)
-
-        category = Category(
-            name=category_data['name'],
-            description=category_data['description'],
-            products=products
-        )
-        categories.append(category)
-
-    return categories
-
-
 if __name__ == "__main__":
 
     print(product1.name)
